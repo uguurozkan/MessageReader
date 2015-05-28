24669434dd70586e29376f1c409fe1c8063c406b
Ugur Ozkan <ugur.ozkan@ozu.edu.tr>
2015-05-28 14:46:09 +0300

    Initial commit

---
+++ app/src/main/java/com/uguurozkan/messagereader/NewMessageNotifier.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2015.
+ * This code belongs to Uğur Özkan
+ * ugur.ozkan@ozu.edu.tr
+ */
+
+package com.uguurozkan.messagereader;
+
+/**
+ * Created by Uğur Özkan on 5/28/2015.
+ * <p/>
+ * ugur.ozkan@ozu.edu.tr
+ */
+public class NewMessageNotifier {
+}
---
+++ app/src/main/java/com/uguurozkan/messagereader/SMSReceiver.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2015.
+ * This code belongs to Uğur Özkan
+ * ugur.ozkan@ozu.edu.tr
+ */
+
+package com.uguurozkan.messagereader;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Uğur Özkan on 5/27/2015.
+ * <p/>
+ * ugur.ozkan@ozu.edu.tr
+ */
+public class SMSReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
